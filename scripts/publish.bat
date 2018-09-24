54b57d4acf39edff5027d8bdb4c429f6026c1585
isierra <iskander.sierra@gmail.com>
2018-09-24 21:29:51 +0200

    Fixed publish script

--- scripts/publish.bat
+++ scripts/publish.bat
@@ -1,18 +1,18 @@
 SETLOCAL
 
-rimraf .vuepress\dist
-build.bat
-rimraf ..\poly-coder-publish
-git clone https://github.com/poly-coder/poly-coder.github.io.git ..\poly-coder-publish
-rimraf ..\poly-coder-publish\*
-xcopy .vuepress\dist ..\poly-coder-publish /E /F /Y
-echo|set /p="Auto-published content from commit " > commitmsg.txt
-git show --format=%H --no-patch >> commitmsg.txt
-cd ..\poly-coder-publish
-git add -A
-git commit -F ..\poly-coder-vuepress\commitmsg.txt
-git push origin
-cd ..\poly-coder-vuepress
-rimraf commitmsg.txt
+call rimraf .vuepress\dist
+call build.bat
+call rimraf ..\poly-coder-publish
+call git clone https://github.com/poly-coder/poly-coder.github.io.git ..\poly-coder-publish
+call rimraf ..\poly-coder-publish\*
+call xcopy .vuepress\dist ..\poly-coder-publish /E /F /Y
+call echo|set /p="Auto-published content from commit " > commitmsg.txt
+call git show --format=%H --no-patch >> commitmsg.txt
+call cd ..\poly-coder-publish
+call git add -A
+call git commit -F ..\poly-coder-vuepress\commitmsg.txt
+call git push origin
+call cd ..\poly-coder-vuepress
+call rimraf commitmsg.txt
 
 ENDLOCAL
